8ae1756ae82d905394a29049b0dbf60b8bc3da58
Viktoriya Sim <simvvit@gmail.com>
2021-03-23 15:49:52 +0300

    feat: implement count cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  if (matrix.length > 0) {
+  let ears = matrix.flatMap(entry =>
+    entry.reduce((counter, ch) =>
+        ch === "^^" ? counter + 1 : counter,
+        0
+    )
+);
+return ears.reduce((total, x) => total + x);
+  } else return 0;
 };
