b82966c0c28fbbba4500fe67c6573687f112df21
Viktoriya Sim <simvvit@gmail.com>
2021-03-25 16:46:15 +0300

    feat: realize dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  let member = [];
+  if(members === null || members === undefined || !Array.isArray(members)) return false;
+  for (i in members) {
+    if (typeof members[i] == 'string') {
+      member.push(members[i].trim().slice(0, 1).toUpperCase());
+    }
+  }
+  return member.sort().join('');
+}
